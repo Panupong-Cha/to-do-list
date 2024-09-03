49c6e7908be2e5c09e78ce8a2433cc146ed4209c
Panupong-Cha <panupongchareyrat@gmail.com>
2024-09-04 01:19:21 +0700

    init project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,121 @@
-import Image from "next/image";
+"use client";
+
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function Home() {
+  const statuses = ["TODO", "DOING", "DONE"];
+  const [status, setStatus] = useState<string>("TODO");
+
+  const [tasks, setTasks] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string | null>(null);
+  const offset = 0;
+  const limit = 30;
+
+  const fetchTasks = async (status: string) => {
+    setLoading(true);
+    setError(null);
+    try {
+      const response = await axios.get(
+        `https://todo-list-api-mfchjooefq-as.a.run.app/todo-list?status=${status}&offset=${offset}&limit=${limit}&sortBy=createdAt&isAsc=true`
+      );
+
+      console.log("API Response:", response.data);
+
+      if (response.data && Array.isArray(response.data.tasks)) {
+        setTasks(response.data.tasks);
+      } else {
+        setError(
+          "Expected an array of tasks, but received a different format."
+        );
+        console.error("Unexpected data format:", response.data);
+      }
+    } catch (error) {
+      console.error("Error fetching tasks:", error);
+      setError("Failed to fetch tasks.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchTasks(status);
+  }, [status]);
+
+  const groupByDate = (tasks: any[]) => {
+    const grouped = tasks.reduce((acc: { [key: string]: any[] }, task) => {
+      const date = task.createdAt.split("T")[0];
+      if (!acc[date]) {
+        acc[date] = [];
+      }
+      acc[date].push(task);
+      return acc;
+    }, {});
+
+    return grouped;
+  };
+
+  const groupedTasks = groupByDate(tasks);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <>
+      <nav className="bg-[#D1E9F6] text-black">
+        <div className="flex items-center justify-center p-5">
+          <h1 className="font-bold text-xl">My Task</h1>
         </div>
+      </nav>
+
+      <div className="flex flex-col items-center p-5">
+        <div className="flex space-x-3 bg-gray-100 rounded-3xl p-2 w-full lg:w-1/2 mb-5">
+          {statuses.map((tab) => (
+            <button
+              key={tab}
+              className={`flex-1 transition-all duration-200 font-bold rounded-3xl p-2 ${
+                status === tab
+                  ? "bg-pink-200 text-black"
+                  : "hover:bg-purple-100 text-gray-400"
+              }`}
+              onClick={() => setStatus(tab)}
+            >
+              {tab}
+            </button>
+          ))}
+        </div>
+
+        {loading ? (
+          <p>Loading...</p>
+        ) : error ? (
+          <p className="text-red-500">{error}</p>
+        ) : Object.keys(groupedTasks).length > 0 ? (
+          Object.keys(groupedTasks).map((date) => (
+            <div key={date} className="w-full lg:w-1/2 mb-4">
+              <div className="font-bold text-base mb-2 text-black">
+                {new Date(date).toUTCString().substring(4, 16)}
+              </div>
+              {groupedTasks[date].map((task: any) => (
+                <div
+                  key={task.id}
+                  className="border rounded-xl p-3 mb-4"
+                  draggable="false"
+                >
+                  <div className="flex justify-between gap-2">
+                    <span className="font-semibold text-black">
+                      {task.title}
+                    </span>
+                    <span className="text-gray-500">
+                      {new Date(task.createdAt).toUTCString().substring(16, 22)}
+                    </span>
+                  </div>
+                  <p className="text-gray-500 text-sm">{task.description}</p>
+                </div>
+              ))}
+            </div>
+          ))
+        ) : (
+          <p>No tasks found.</p>
+        )}
       </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </>
   );
 }
